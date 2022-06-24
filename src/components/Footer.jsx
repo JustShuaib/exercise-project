1ac0e0bab7a89ae5793acdb679338abb392daaa3
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-06-24 23:16:52 +0100

    Done  with the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,31 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Stack, Typography } from "@mui/material";
 import Logo from "../assets/icons/logo.png";
 
-const Footer = () => {
-  return (
-    <Box mt="3rem" bgcolor="#fff3f4">
-      <Stack alignItems="center" px="5rem" pt="3rem">
-        <img src={Logo} alt="logo" width="80rem" />
-        <Typography variant="h5" pb="3rem" mt="2rem">
-          Made with ❤️by{" "}
-          <a
-            href="www.github.com/JustShuaib"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Adeoti Shuaib
-          </a>
-        </Typography>
-      </Stack>
-    </Box>
-  );
-};
+const Footer = () => (
+  <Stack
+    alignItems="center"
+    justifyContent="center"
+    direction={{ xs: "column", sm: "row" }}
+    gap={{ xs: 2, sm: 4 }}
+    mt="3rem"
+    py="3rem"
+    bgcolor="#fff3f4"
+  >
+    <Link to="/">
+      <img src={Logo} alt="logo" width="80rem" />
+    </Link>
+    <Typography variant="h5" textAlign="center">
+      Made with ❤️by{" "}
+      <a
+        href="www.github.com/JustShuaib"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Adeoti Shuaib
+      </a>
+    </Typography>
+  </Stack>
+);
 
 export default Footer;
