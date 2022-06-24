1d7e871926bae4b96f3b3faad5e8d7ebade74902
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-06-24 23:58:56 +0100

    done with nav bar and banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,38 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 import Logo from "../assets/icons/logo.png";
-const Navbar = () => {
-  return (
-    <Stack
-      direction="row"
-      justifyContent="space-around"
-      alignItems="center"
-      sx={{
-        gap: { sm: "122px", xs: "0" },
-        mt: { sm: "32px", xs: "20px" },
-        justifyContent: "none",
-      }}
-      px="20px"
-    >
-      <Link to="/">
-        <img
-          src={Logo}
-          alt="logo"
-          style={{ width: "48px", height: "48px", margin: "0 20px" }}
-        />
+const Navbar = () => (
+  <Stack
+    direction="row"
+    justifyContent="space-between"
+    alignItems="center"
+    mt={{ xs: "1.5rem", sm: "2rem" }}
+    px="1.4rem"
+    width={{ xs: "100%", lg: "35rem" }}
+  >
+    <Link to="/">
+      <img
+        src={Logo}
+        alt="logo"
+        style={{ width: "3rem", height: "3rem", marginLeft: "1.4rem" }}
+      />
+    </Link>
+    <Stack direction="row" gap="2.5rem" fontSize="1.5rem">
+      <Link
+        to="/"
+        style={{
+          textDecoration: "none",
+          color: "#3a1212",
+          borderBottom: "3px solid #ff2625",
+        }}
+      >
+        Home
       </Link>
-      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
-        <Link
-          to="/"
-          style={{
-            textDecoration: "none",
-            color: "#3a1212",
-            borderBottom: "3px solid #ff2625",
-          }}
-        >
-          Home
-        </Link>
-        <a
-          href="#exercises"
-          style={{ textDecoration: "none", color: "#3a1212" }}
-        >
-          Exercises
-        </a>
-      </Stack>
+      <a href="#exercises" style={{ textDecoration: "none", color: "#3a1212" }}>
+        Exercises
+      </a>
     </Stack>
-  );
-};
+  </Stack>
+);
 
 export default Navbar;
