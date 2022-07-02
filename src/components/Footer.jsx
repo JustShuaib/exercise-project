12d57195c7676ad3f3b0c9b34d8c70c5db2dae88
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-07-02 17:19:58 +0100

    increased footer font on desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,11 @@ const Footer = () => (
     <Link to="/">
       <img src={Logo} alt="logo" width="80rem" />
     </Link>
-    <Typography fontSize="1.4rem" textAlign="center" fontFamily="Josefin Sans">
+    <Typography
+      fontSize={{ xs: "1.4rem", lg: "1.6rem" }}
+      textAlign="center"
+      fontFamily="Josefin Sans"
+    >
       Made with ❤️ by{" "}
       <a
         href="www.github.com/JustShuaib"
