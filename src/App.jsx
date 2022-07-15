719b7224e50a2e839ffa14f36c881417a484871c
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-07-15 17:53:38 +0100

    testing App

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,23 @@ import Home from "./routes/Home";
 import ExerciseDetail from "./routes/ExerciseDetail";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { ThemeProvider } from "@mui/material";
 const App = () => (
   <>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
-    </Routes>
-    <Footer />
+    <ThemeProvider
+      theme={{
+        typography: {
+          fontFamily: "Josefin Sans",
+        },
+      }}
+    >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </ThemeProvider>
   </>
 );
 export default App;
